20bbe9dac2b0f9688ca869916786eb847b7ebd0d
jbxamora <jbxamora@icloud.com>
2023-02-06 18:57:23 -0800

    added db folder

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+-- For volunteer hours tracking:
+
+-- Volunteer table: stores information about each volunteer, including name, email, and hours worked.
+
+-- Project table: stores information about each project, including project name and description.
+
+-- Volunteer-Project table: associates volunteers with the projects they have worked on and the hours they have contributed.
+
+-- For donation tracking:
+
+-- Donor table: stores information about each donor, including name, email, and payment information.
+
+-- Donation table: stores information about each donation, including date, amount, and payment method.
+
+-- For impact metrics:
+
+-- Project table: stores information about each project, including project name, description, and impact metrics.
+
+-- Metric table: stores information about the impact metrics, including the name of the metric, the value, and the project it is associated with.
