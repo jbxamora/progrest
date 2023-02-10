a49ab6b9324d0eb24c2ff701f63fd64955c1b2fd
jbxamora <jbxamora@icloud.com>
2023-02-09 20:26:43 -0800

    created impacts model

--- models/impact.js
+++ models/impact.js
@@ -1,1 +1,33 @@
 //donation impact
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+ class Impact extends Model {}
+
+ Impact.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        desc: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        outreach: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'impact',
+    }
+ );
+// Later add more metrics - connect a see more page for impacts
+ module.exports = Impact;
