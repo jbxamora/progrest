edc4d398a891a5b58dcd3f57ca85c37dfc515c07
jbxamora <jbxamora@icloud.com>
2023-02-09 21:32:22 -0800

    updated models

--- models/volunteer.js
+++ models/volunteer.js
@@ -42,9 +42,21 @@ Volunteer.init(
   },
 );
 
-Volunteer.sum('hours').then(sum => {
-  res.render('portal', { Hours })
+// Volunteer.sum('hours').then(sum => {
+//   res.render('portal', { Hours })
+// })
+
+Volunteer.findAll({
+  attributes: [
+    'project',
+    [sequelize.fn('sum', sequelize.col('hours')), 'totalHours']
+  ],
+  group: ['project']
 })
+  .then(results => {
+    res.render('portal', { results });
+  });
+
 
 Volunteer.count().then(count => {
   res.render('portal', { Volunteers });
