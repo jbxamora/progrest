3f74b798f75550190f30e62ea9068ea2503d1d6b
jbxamora <jbxamora@icloud.com>
2023-02-13 19:46:53 -0800

    Updated authentication helper.

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,28 @@
-const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
-    if (!req.session.logged_in) {
-      res.redirect('/login');
+const withAuth = async (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+
+    // Check if user exists in the database
+    const { email, password } = req.session;
+    const user = await user.findOne({ where: { email } });
+    if (!user) {
+      // If the user doesnt exist, throw error
+      res.status(400).json({ message: 'Incorrect email or password' });
     } else {
-      next();
+      // If the user exists, check if the password matches
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (validPassword) {
+        // If the password is correct, continue with the request
+        next();
+      } else {
+        // If the password is incorrect, throw error
+        res.status(400).json({ message: 'Incorrect email or password' });
+      }
     }
-  };
-  
-  module.exports = withAuth;
-  
+  }
+};
+
+
+module.exports = withAuth;
